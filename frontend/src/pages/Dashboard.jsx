ffcaeec56d1337add8551da30f6ab1a7897bd4fc
FontouraC18H22O2 <hugo.fontouradias@gmail.com>
2026-06-04 20:04:43 +0100

    feat:repositiro novo

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Dashboard() {
+  return (
+    <div className="min-h-screen bg-neutral-950 text-white flex items-center justify-center">
+      <h2 className="text-2xl font-bold text-fitnessGym">Painel do Personal Trainer (Brevemente)</h2>
+    </div>
+  );
+}
