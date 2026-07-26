5751680ef1d2a71bdcd4c70abb6505478d1140f6
FontouraC18H22O2 <hugo.fontouradias@gmail.com>
2026-07-26 13:54:17 +0100

    feat:Add vercel web analystie

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,7 @@ import AccessRequests from "./pages/AccessRequests";
 import Perfil from "./pages/Perfil";
 import AvaliacaoFisica from "./pages/AvaliacaoFisica";
 import Templates from "./pages/Templates";
+import { Analytics } from '@vercel/analytics/react';
 
 
 function App() {
@@ -49,6 +50,7 @@ function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
+      <Analytics /> 
     </AuthProvider>
   );
 }
